e865c903adce42bfab4d4c7f04c8b44addb01d18
Stephen Corcoran <stephenc4815@gmail.com>
2021-01-11 20:25:07 +0000

    Added new functions to check metrics and reorganised code

--- src/index.js
+++ src/index.js
@@ -1,28 +1,58 @@
 require('dotenv').config();
 
 const { google } = require('googleapis');
+let videos = [];
 
-google
-  .youtube('v3')
-  .search.list({
-    key: process.env.YOUTUBE_TOKEN,
-    part: 'snippet',
-    q: 'software developer',
-    maxResults: 50,
-    publishedAfter: calculatePublishAfterDate(),
-  })
-  .then((response) => {
-    const { data } = response;
-    data.items.forEach((item) => {
-      //loop through each video and check the metrics
-      console.log('====================================');
-      console.log(item.snippet.title);
-      console.log('====================================');
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+main();
+
+function main() {
+  getYoutubeVideos();
+  videos.forEach((video) => {});
+  // getViewCount();
+}
+
+function getYoutubeVideos() {
+  google
+    .youtube('v3')
+    .search.list({
+      key: process.env.YOUTUBE_TOKEN,
+      part: 'snippet',
+      q: 'software developer',
+      maxResults: 2,
+      publishedAfter: calculatePublishAfterDate(),
+    })
+    .then((response) => {
+      const { data } = response;
+      data.items.forEach((item) => {
+        //videoId, channelId, title
+        videos.push({
+          videoId: item.id.videoId,
+          channelId: item.snippet.channelId,
+          title: item.snippet.title,
+          viewCount: getViewCount(item.id.videoId),
+        });
+        console.log(videos);
+      });
+    })
+    .catch((err) => console.log(err));
+}
+
+function getViewCount(videoId) {
+  google
+    .youtube('v3')
+    .videos.list({
+      key: process.env.YOUTUBE_TOKEN,
+      id: videoId,
+      part: 'statistics',
+    })
+    .then((response) => {
+      const { data } = response;
+      data.items.forEach((item) => {
+        // return 'TESTING VIEW COUNT';
+      });
+    })
+    .catch((err) => console.log(err));
+}
 
 function calculatePublishAfterDate() {
   //creates Date object with today's date
@@ -35,4 +65,64 @@ function calculatePublishAfterDate() {
   let year = date.getFullYear();
 
   return `${year}-${month}-${day}T00:00:00Z`;
+}
+
+function getVideoInfo(videoId) {
+  google
+    .youtube('v3')
+    .videos.list({
+      key: process.env.YOUTUBE_TOKEN,
+      id: videoId,
+      part: 'statistics',
+    })
+    .then((response) => {
+      const { data } = response;
+      data.items.forEach((item) => {
+        //loop through each video and check the metrics
+        console.log('====================================');
+        console.log(item.id.channelId);
+        console.log('====================================');
+        calculateMetrics(item.statistics);
+      });
+    })
+    .catch((err) => console.log(error));
+}
+
+function calculateMetrics(statistics, channelId) {
+  //TODO - Implement Functions Below
+  hasValidViewCount(statistics.viewCount);
+  getSubscriberCount(channelId);
+  // hasValidSubscriberCount();
+  // hasValidViewsToSubscriberRatio();
+}
+
+function hasValidViewCount(viewCount) {
+  return viewCount > 500;
+}
+
+function getSubscriberCount(channelId) {
+  google
+    .youtube('v3')
+    .channels.list({
+      key: process.env.YOUTUBE_TOKEN,
+      id: channelId,
+      part: 'statistics',
+    })
+    .then((response) => {
+      const { data } = response;
+      data.items.forEach((item) => {
+        //loop through each video and check the metrics
+      });
+    })
+    .catch((err) => console.log(error));
+}
+
+function hasValidSubscriberCount(subscriberCount) {
+  return subscriberCount > 100;
+}
+
+function hasValidViewsToSubscriberRatio(viewCount, subscriberCount) {
+  let ratio = viewCount / subscriberCount;
+  let score = viewCount * min(ration, 5);
+  return true;
 }
