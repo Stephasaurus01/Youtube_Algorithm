5938b42eebd7a9ffb2ebe4eb8d7d60213c459156
Stephen <stephenc4815@gmail.com>
2021-01-27 16:22:20 +0000

    Split up code into modules

--- src/index.js
+++ src/index.js
@@ -1,35 +1,20 @@
-require('dotenv').config();
-const calculateMetrics = require('./metrics.js');
-const emailVideos = require('./email.js');
-const getVideos = require('./youtube.js');
-
-
-
+require("dotenv").config();
+const calculateMetrics = require("./metrics.js");
+const emailVideos = require("./email.js");
+const getVideos = require("./youtube.js");
 
 main();
 
 async function main() {
-  let emailText = '';
-  let videos = getVideos();
-  videos.then((ret) => {
-    console.log('ret is: ')
-    console.log(ret)
-  })
-  
-  // getSubscriberCount()
-  //   .then(getVideoStatistics)
-  //   .then(async () => {
-  //     await calculateMetrics(videos);
-  //     videos.forEach((video) => {
-  //       emailText += `https://www.youtube.com/watch?v=${video.videoId}\n`;
-  //       console.log(`https://www.youtube.com/watch?v=${video.videoId}`);
-  //     });
-  //     emailVideos(emailText);
-    // });
+  let emailText = "";
+  let videos = await getVideos();
+
+  videos = calculateMetrics(videos);
+
+  videos.forEach((video) => {
+    emailText += `https://www.youtube.com/watch?v=${video.videoId}\n`;
+    console.log(`https://www.youtube.com/watch?v=${video.videoId}`);
+  });
+
+  emailVideos(emailText);
 }
-
-
-
-
-
-
--- src/metrics.js
+++ src/metrics.js
@@ -1,33 +1,33 @@
-module.exports =  async function calculateMetrics(videos) {
-    videos.forEach((video) => {
-      let validVideo = true;
-      if (!hasValidViewCount(video.viewCount)) {
-        validVideo = false;
-      }
-      if (!hasValidSubscriberCount(video.subscriberCount)) {
-        validVideo = false;
-      }
-      if (!hasValidDuration(video.durationInSeconds)) {
-        validVideo = false;
-      }
-  
-      if (!validVideo) {
-        let key = video.videoId;
-        videos.delete(key);
-      }
-    });
-  }
-  
-  function hasValidViewCount(viewCount) {
-    return viewCount > 50 && viewCount < 5000;
-  }
-  
-  function hasValidSubscriberCount(subscriberCount) {
-    return subscriberCount > 200;
-  }
-  
-  function hasValidDuration(duration) {
-    return duration > 120;
-  }
+module.exports = function calculateMetrics(videos) {
+  let vids = videos;
+  vids.forEach((video) => {
+    let validVideo = true;
+    if (!hasValidViewCount(video.viewCount)) {
+      validVideo = false;
+    }
+    if (!hasValidSubscriberCount(video.subscriberCount)) {
+      validVideo = false;
+    }
+    if (!hasValidDuration(video.durationInSeconds)) {
+      validVideo = false;
+    }
 
-  
+    if (!validVideo) {
+      let key = video.videoId;
+      vids.delete(key);
+    }
+  });
+  return vids;
+};
+
+function hasValidViewCount(viewCount) {
+  return viewCount > 50 && viewCount < 5000;
+}
+
+function hasValidSubscriberCount(subscriberCount) {
+  return subscriberCount > 200;
+}
+
+function hasValidDuration(duration) {
+  return duration > 120;
+}
